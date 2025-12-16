72c8831a95967e9d06d2b8922acdcea2f8d15d35
jeli01 <jeli01@naver.com>
2025-12-16 19:25:49 +0900

    chore: swagger 설정 완료

---
+++ src/main/java/io/github/jeli01/practice/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package io.github.jeli01.practice.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Practice API 문서")
+                .description("연습 프로젝트 스웨거입니다.")
+                .version("1.0.0");
+    }
+}
